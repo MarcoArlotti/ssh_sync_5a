3a34760af0880653f84b1719eefd0b639872d7ff
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2025-12-16 09:40:47 +0100

    funcional exercise 13

---
+++ arlotti_13/app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask,render_template
+
+def create_app():
+    # 1. Creiamo l'istanza di Flask
+    # instance_relative_config=True dice a Flask: 
+    # "Cerca la cartella 'instance' fuori da 'app', non dentro."
+    app = Flask(__name__, instance_relative_config=True)
+
+    # 2. Configurazione di base
+    # Qui impostiamo le variabili fondamentali.
+    app.config.from_mapping(
+        # SECRET_KEY serve a Flask per firmare i dati sicuri (es. sessioni).
+        # 'dev' va bene per sviluppare, ma in produzione andrà cambiata.
+        SECRET_KEY='dev',
+        # Diciamo a Flask dove salvare il file del database SQLite
+        DATABASE=os.path.join(app.instance_path, 'blog.sqlite'),
+    )
+
+    # 3. Assicuriamoci che la cartella 'instance' esista fisicamente.
+    # Se non esiste (es. è la prima volta che avvii), Flask la crea.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+    @app.route('/')
+    def hello():
+        title = "Pagina Principale"
+        message = "we"
+        name = "FABIO"
+        return render_template('home.html', title=title, message=message, name=name)
+    return app
